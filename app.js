b69f072403e59a2524ececec663da863edcc917c
Asif Bin Kabir <asifbinkabircse@gmail.com>
2023-12-29 17:03:12 +0600

    Check the server using get request

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const axios = require("axios");
 const app = express();
 const port = 3000;
 
+app.get("/hello", function(req, res) {
+    res.send("HELLO");
+});
+
 app.listen(port, function () {
     console.log(`Server is running at http://localhost:${port}`);
 });
